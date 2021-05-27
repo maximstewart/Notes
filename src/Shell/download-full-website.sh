ab7da95848f74c14f575af232ed0809d0e21e2e6
itdominator <1itdominator@gmail.com>
2021-05-27 18:54:02 -0500

    added website downloader script

---
+++ src/Shell/download-full-website.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# . CONFIG.sh
+
+# set -o xtrace       ## To debug scripts
+# set -o errexit      ## To exit on error
+# set -o errunset     ## To exit if a variable is referenced but not set
+
+
+function main() {
+    SCRIPTPATH="$( cd "$(dirname "")" >/dev/null 2>&1 ; pwd -P )"
+    cd "${SCRIPTPATH}"
+    echo "Working Dir: " $(pwd)
+
+    wget \
+         --recursive \
+         --no-clobber \
+         --page-requisites \
+         --html-extension \
+         --convert-links \
+         --restrict-file-names=windows \
+         --domains schlechtwetterfront.github.io \
+         --no-parent \
+             "schlechtwetterfront.github.io/ze_filetypes/index.html"
+}
+main $@;
